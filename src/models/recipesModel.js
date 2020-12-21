ef8245885c253e0938b327608e4b94cd19be7d23
hendrasolih <hendra.solih.jp@gmail.com>
2020-12-21 19:21:20 +0700

    add recipe feature

--- src/models/recipesModel.js
+++ src/models/recipesModel.js
@@ -19,3 +19,36 @@ exports.getAllRecipesModel = (req) => {
     });
   });
 };
+
+exports.postNewRecipe = (req) => {
+  // mendapat objek request dari client
+  // melakukan query ke db
+  // mengirim response
+  //const img = process.env.SERVER + "/images/" + req.file.filename; for single
+  const image = JSON.stringify(
+    req.files.img.map((e) => process.env.SERVER + "/images/" + e.filename)
+  );
+  const videos = JSON.stringify(
+    req.files.videos.map((e) => process.env.SERVER + "/videos/" + e.filename)
+  );
+  const { body } = req;
+  console.log(req.files);
+  const insertBody = {
+    ...body,
+    created_at: new Date(Date.now()),
+    updated_at: new Date(Date.now()),
+    //prd_image: img,
+    img_rcp: image,
+    video_rcp: videos,
+  };
+  return new Promise((resolve, reject) => {
+    const qs = "INSERT INTO recipes SET ?";
+    db.query(qs, insertBody, (err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -2,6 +2,9 @@ const recipesRouter = require("express").Router();
 
 const recipesCtrl = require("../controllers/recipesCtrl");
 
+const multiUpload = require("../helpers/middlewares/multiUpload");
+
 recipesRouter.get("/", recipesCtrl.getAllRecipes);
+recipesRouter.post("/", multiUpload, recipesCtrl.postNewRecipeCtrl);
 
 module.exports = recipesRouter;
