b6f5776ec7f5affa99b1db2d304a7c77c0794e67
hendrasolih <hendra.solih.jp@gmail.com>
2020-12-21 21:04:48 +0700

    add get product by id

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const express = require("express");
 const mainRouter = express.Router();
 
 const recipesRouter = require("./recipesRoutes");
+const recipeByIdRouter = require("./recipeByIdRoutes");
 const authRouter = require("./authRoutes");
 
 mainRouter.use("/recipes", recipesRouter); // localhost:5000/recipes
+mainRouter.use("/recipe", recipeByIdRouter); // localhost:5000/recipes
 mainRouter.use("/auth", authRouter); // localhost:5000/auth
 
 module.exports = mainRouter;
