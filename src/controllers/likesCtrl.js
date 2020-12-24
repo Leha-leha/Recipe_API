0ab6ee862ea04516fda584a8f20301fce01c87c4
hendrasolih <hendra.solih.jp@gmail.com>
2020-12-24 13:55:44 +0700

    add get recipes like by user

--- src/controllers/likesCtrl.js
+++ src/controllers/likesCtrl.js
@@ -16,4 +16,23 @@ module.exports = {
         res.json(err);
       });
   },
+
+  getRecipeLike: (req, res) => {
+    likesModel
+      .getRecipeLike(req)
+      .then((data) => {
+        if (data.length) {
+          res.json({
+            data,
+          });
+        } else {
+          res.status(404).json({
+            msg: "Data not Found",
+          });
+        }
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  },
 };
--- src/models/likesModel.js
+++ src/models/likesModel.js
@@ -12,3 +12,18 @@ exports.postNewLike = (body) => {
     });
   });
 };
+
+exports.getRecipeLike = (req) => {
+  const { id } = req.params;
+  return new Promise((resolve, reject) => {
+    const qs =
+      "SELECT r.img_rcp, r.title_rcp FROM recipes as r JOIN likes as l ON l.recipe_id=r.id_rcp WHERE l.user_id=?";
+    db.query(qs, id, (err, data) => {
+      if (!err) {
+        resolve(data);
+      } else {
+        reject(errr);
+      }
+    });
+  });
+};
