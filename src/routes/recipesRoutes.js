2942aae5fb576cb4683a8f6acaf2a843c206fe94
hendrasolih <hendra.solih.jp@gmail.com>
2020-12-24 13:18:13 +0700

    get all recipe by user id

--- src/routes/recipesRoutes.js
+++ src/routes/recipesRoutes.js
@@ -6,5 +6,6 @@ const multiUpload = require("../helpers/middlewares/multiUpload");
 
 recipesRouter.get("/", recipesCtrl.getAllRecipes);
 recipesRouter.post("/", multiUpload, recipesCtrl.postNewRecipeCtrl);
+recipesRouter.get("/:id", recipesCtrl.getAllRecipesById);
 
 module.exports = recipesRouter;
