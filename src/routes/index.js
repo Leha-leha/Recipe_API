0b71d20844afc788c3f13054071bec0790333088
hendrasolih <hendra.solih.jp@gmail.com>
2020-12-24 11:09:44 +0700

    get user by id

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ const searchRouter = require("./searchRoutes");
 const commentsRouter = require("./commentsRoutes");
 const savesRouter = require("./savesRoutes");
 const likesRouter = require("./likesRoutes");
+const userRouter = require("./userRoutes");
 
 mainRouter.use("/recipes", recipesRouter); // localhost:5000/recipes
 mainRouter.use("/recipe", recipeByIdRouter); // localhost:5000/recipes
@@ -17,5 +18,6 @@ mainRouter.use("/search", searchRouter); // localhost:5000/search
 mainRouter.use("/comments", commentsRouter); // localhost:5000/comments
 mainRouter.use("/saves", savesRouter); // localhost:5000/saves
 mainRouter.use("/likes", likesRouter); // localhost:5000/likes
+mainRouter.use("/user", userRouter); // localhost:5000/user
 
 module.exports = mainRouter;
